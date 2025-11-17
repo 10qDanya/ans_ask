f73ccd7aa8b4bf8317e89b63f477455c1ef3124d
10qDanya <lumusyt@gmail.com>
2025-11-17 17:22:44 +0300

    ready questions

---
+++ questions/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from questions.apps import QuestionsConfig
+from questions.views import QuestionListApiView, QuestionCreateApiView, QuestionRetrieveApiView, QuestionDestroyApiView
+
+app_name = QuestionsConfig.name
+
+urlpatterns = [
+    path('', QuestionListApiView.as_view(), name='questions_list'),
+    path('create/', QuestionCreateApiView.as_view(), name='question_create'),
+    path('retrieve/<int:pk>/', QuestionRetrieveApiView.as_view(), name='question_retrieve'),
+    path('destroy/<int:pk>/', QuestionDestroyApiView.as_view(), name='question_destroy'),
+]
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 from .models import Question
+from rest_framework import generics
+from questions.serializers import QuestionSerializer, QuestionRetrieveSerializer
 
+class QuestionListApiView(generics.ListAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionCreateApiView(generics.CreateAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionRetrieveApiView(generics.RetrieveAPIView):
+    
+    queryset = Question.objects.all()
+    serializer_class = QuestionRetrieveSerializer
+
+class QuestionDestroyApiView(generics.DestroyAPIView):
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
